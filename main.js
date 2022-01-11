6918ce18f98eb9f1edc1692e4fb8a18e99329a30
Sergo_Kaz <wwe619dx@ukr.net>
2022-01-11 21:34:52 +0200

    finish commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const {form} = document.forms;
+
+form.addEventListener('submit', formSend);
+
+async function formSend(e) {
+  e.preventDefault();
+
+  const formDate = new FormData(form);
+  const values = Object.fromEntries(formDate.entries());
+
+  console.log(JSON.stringify(values));
+}
